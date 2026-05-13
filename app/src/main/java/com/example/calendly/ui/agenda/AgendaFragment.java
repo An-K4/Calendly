bf04dd4cf77bd4274a29406abf248631bf3c67f8
An-K4 <ana5k44nknd@gmail.com>
2026-05-13 19:44:47 +0700

    fix: reload agenda when click different year

--- app/src/main/java/com/example/calendly/ui/agenda/AgendaFragment.java
+++ app/src/main/java/com/example/calendly/ui/agenda/AgendaFragment.java
@@ -5,11 +5,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.calendly.data.repository.EventRepository;
@@ -44,6 +44,7 @@ public class AgendaFragment extends Fragment implements AgendaContract.View {
 
         yearAdapter = new YearAdapter(new ArrayList<>(), year -> {
             presenter.loadYears(year);
+            presenter.loadAgendaByYear(year);
         });
         binding.rvYears.setLayoutManager(new LinearLayoutManager(
                 getContext(),
@@ -53,7 +54,8 @@ public class AgendaFragment extends Fragment implements AgendaContract.View {
         binding.rvYears.setAdapter(yearAdapter);
 
         agendaAdapter = new AgendaAdapter(new ArrayList<>(), item -> {
-
+            // in development
+            Toast.makeText(getContext(), "In development", Toast.LENGTH_SHORT).show();
         });
         binding.rvAgenda.setLayoutManager(new LinearLayoutManager(getContext()));
         binding.rvAgenda.setAdapter(agendaAdapter);
